413bf422a4148b2d4c43d2884ff2a4cced79a37b
Alizamora <39492909+Alizamora@users.noreply.github.com>
2018-07-04 12:42:28 -0600

    Practica #3
    Subo la practica #3.

---
+++ practica #3/scritp.js
@@ -0,0 +1,16 @@
+/* 
+    Practica #3
+*/
+
+//Declaro las variables
+let suma = 0;
+let numero = 2;
+
+//Pongo mi condicion 
+while (numero < 1000) {
+    suma = numero + suma;
+    numero = numero + 2;
+}
+
+//imprimo la suma en la consola
+console.log (suma);
